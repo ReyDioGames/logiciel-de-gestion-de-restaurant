e4f5f591875d04935ea52d4c8210a677fd81587c
RAHAL Rayane <91078977+ReyDioGames@users.noreply.github.com>
2023-02-11 15:11:47 +0100

    Add files via upload

---
+++ CafeteriaResto.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cafeteriaresto;
+
+import static java.awt.Frame.MAXIMIZED_BOTH;
+
+
+/**
+ *
+ * @author Rayane
+ */
+public class CafeteriaResto {
+
+    
+        
+        
+        /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Vente v = new Vente();
+        v.setVisible(true);
+        v.setExtendedState(MAXIMIZED_BOTH);
+        
+    }
+    
+}
